3eb06cef7b0cca8fcfed39b91fbd0c005487cd8a
Dominique Miller <dominique.p.miller@gmail.com>
2018-12-27 15:17:15 -0500

    Custom error object for api errors

--- StarterTemplate/enums/ApiErrorType.swift
+++ StarterTemplate/enums/ApiErrorType.swift
@@ -4,9 +4,11 @@ import Foundation
 // add additional error types that
 // the API could return, Usually found
 // in exceptions.rb in API
-enum ApiError: Error {
+enum ApiErrorType: Error {
     case notFound
+    case badRequest
     case unauthorized
     case internalServerError
     case recordInvalid
+    case unknown
 }
